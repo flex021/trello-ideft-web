37be387a863dcfe006133515485e938efa149791
flex021 <congthong2015@gmail.com>
2024-10-01 22:26:11 +0700

    fix process env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
 //constants
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-ideft-api.onrender.com'
+}
+
+export const API_ROOT = apiRoot
